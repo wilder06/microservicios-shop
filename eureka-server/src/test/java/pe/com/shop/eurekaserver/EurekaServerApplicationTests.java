db8dea4a50a29bfcf085081404880cdbeb9d53a6
wilder06 <a1310116@gmail.com>
2023-03-19 14:45:27 -0500

    initial commit

---
+++ eureka-server/src/test/java/pe/com/shop/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.shop.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
